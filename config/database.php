b3f2997e38976a0510489069d37949c5c4541a73
unknown <momlaouali@gmail.com>
2025-09-13 14:50:22 +0100

    re

--- config/database.php
+++ config/database.php
@@ -14,7 +14,16 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
+    'mysql' => [
+        'driver' => 'mysql',
+        'url' => env('DATABASE_URL'),
+        'host' => env('DB_HOST', '127.0.0.1'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        // ...
+    ],
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
